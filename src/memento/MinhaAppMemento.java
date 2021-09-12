49dfaa340be084d3d33142505b552d8d9d3619c4
Vanessa Pessoal <vanessapessoal@PPM-VIX-1979.local>
2021-09-12 10:17:46 -0300

    Adicionando padrao de projeto memento

---
+++ src/memento/MinhaAppMemento.java
@@ -0,0 +1,25 @@
+package memento;
+
+public class MinhaAppMemento {
+    public static void main(String[] args) {
+        //Cria objeto vendedor
+        Vendedor vendedor = new Vendedor("Joao", 10000.0F);
+
+        System.out.println(vendedor.getNome() + " - Total de vendas: " + vendedor.getTotalVendas() + "\n");
+
+        //Salva estado interno
+        VendasMemory memory = new VendasMemory();
+        memory.setMemento(vendedor.createMemento());
+
+        //Altera os valores dos atributos do objeto vendedor
+        vendedor.setNome("Pedro");
+        vendedor.setTotalVendas(50000.0F);
+
+        System.out.println(vendedor.getNome() + " - Total de vendas: " + vendedor.getTotalVendas() + "\n");
+
+        //Restaura memento
+        vendedor.retoreMemeneto(memory.getMemento());
+
+        System.out.println(vendedor.getNome() + " - Total de vendas: " + vendedor.getTotalVendas() + "\n");
+    }
+}
---
+++ src/memento/VendasMemory.java
@@ -0,0 +1,14 @@
+package memento;
+
+public class VendasMemory {
+
+    private Memento memento;
+
+    public Memento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(Memento memento) {
+        this.memento = memento;
+    }
+}
